dc50cf05607d1b74b49d33d6b4f8a90d238a96f0
AlexK <alexander.kutkanych@gmail.com>
2017-07-02 14:49:26 +0300

    concate js added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');  
+var rename = require('gulp-rename');  
+var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
 // var browserSync = require('browser-sync').create();
@@ -18,7 +21,17 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('css/dest'));
 });
 
+//script paths
+var jsFiles = 'js/src/*.js',  
+    jsDest = 'js/';
+
+gulp.task('concat-js', function() {  
+    return gulp.src(jsFiles)
+        .pipe(concat('script.main.js'))
+        .pipe(gulp.dest(jsDest));
+});
+
 //Watch task
 gulp.task('watch',function() {
-    gulp.watch(['css/src/*.scss', 'css/dest/*.css', 'css/*.css'], ['concat-css', 'sass']);
+    gulp.watch(['css/src/*.scss', 'css/dest/*.css', 'css/*.css', 'js/src/*.js'], ['concat-css', 'sass', 'concat-js']);
 });
