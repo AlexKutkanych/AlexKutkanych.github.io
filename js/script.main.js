e4a702a352afe4537f62e63a1a3bc910238727f0
AlexK <alexander.kutkanych@gmail.com>
2017-07-03 23:26:24 +0300

    300*300 slide gallery ready, twitter testing

--- js/script.main.js
+++ js/script.main.js
@@ -29,6 +29,7 @@ $(document).ready(function(){
 
 });
 
+//booking block
 
 var bookingBtn = document.querySelector("#booking__btn"),
     bookingModalBlock = document.querySelector(".header-wrapper__booking-section"),
@@ -267,6 +268,41 @@ function initMap() {
     map: map
   });
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+	showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+	var i;
+	var slides = document.querySelectorAll(".carousel__slide");
+	var slidesLength = slides.length;
+	if (n > slidesLength) {
+		slideIndex = 1;
+	} else if (n < 1) {
+		slideIndex = slidesLength;
+	}
+	for (i = 0; i < slidesLength; i++){
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "block";
+}
+
+var prevSlideBtn = document.querySelector(".carousel__pagination_prev");
+var nextSlideBtn = document.querySelector(".carousel__pagination_next");
+
+prevSlideBtn.addEventListener("click", function(e){
+	e.preventDefault();
+	plusSlides(-1);
+});
+
+nextSlideBtn.addEventListener("click", function(e){
+	e.preventDefault();
+	plusSlides(1);
+});
 
 /*
  * jQuery JavaScript Library v1.3.2
